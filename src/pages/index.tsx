1af2cea764aaaea139b51118c99e7508b43b9651
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2024-04-02 23:48:15 +0000

    aaa aaaa aaaa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalContent,
-  ModalBody,
-  useDisclosure,
-} from "@nextui-org/react";
+import { useDisclosure } from "@nextui-org/react";
 import { useRouter } from "next/router";
 
 export type CellColor = "red" | "orange" | "yellow" | "green";
@@ -21,21 +14,24 @@ export interface GridCell {
 export const gridSize = { rows: 9, cols: 12 };
 
 export const initialGridState = (): GridCell[][] => {
-  let id = 0;
+  let cellId = 0;
+  const totalCells = gridSize.rows * gridSize.cols;
+  const initialProbability = 1 / totalCells;
+
   return Array.from({ length: gridSize.rows }, () =>
     Array.from({ length: gridSize.cols }, () => ({
-      id: id++,
-      probability: 1 / (gridSize.rows * gridSize.cols),
+      id: cellId++,
+      probability: initialProbability,
       direction: "",
     }))
   );
 };
 
-export const placeGhost = (): [number, number] => {
-  const x = Math.floor(Math.random() * gridSize.cols);
-  const y = Math.floor(Math.random() * gridSize.rows);
-  console.log("Ghost placed at", x, y);
-  return [x, y];
+export const placeGhost = () => {
+  const xg = Math.floor(Math.random() * gridSize.cols);
+  const yg = Math.floor(Math.random() * gridSize.rows);
+  console.log("Ghost placed at", xg, yg);
+  return [xg, yg];
 };
 
 export const manhattanDistance = (
@@ -58,7 +54,7 @@ interface ColorProbability {
 
 const conditionalColorProbabilities: ColorProbability = {
   "0": { red: 0.8, orange: 0.1, yellow: 0.05, green: 0.05 },
-  "1": { red: 0.15, orange: 0.65, yellow: 0.15, green: 0.05 },
+  "1": { red: 0.05, orange: 0.75, yellow: 0.15, green: 0.05 },
   "2": { red: 0.1, orange: 0.7, yellow: 0.15, green: 0.05 },
   "3": { red: 0.05, orange: 0.15, yellow: 0.65, green: 0.15 },
   "4": { red: 0.05, orange: 0.05, yellow: 0.7, green: 0.2 },
@@ -72,15 +68,17 @@ export const DistanceSense = (
   clickX: number,
   clickY: number
 ): GridCell[][] => {
-  const distance = manhattanDistance(clickX, clickY, ghostX, ghostY);
-  const color = sampleColorFromDistance(distance);
-  return grid.map((row, y) =>
-    row.map((cell, x) =>
-      x === clickX && y === clickY ? { ...cell, color } : cell
+  const gridDistance = manhattanDistance(clickX, clickY, ghostX, ghostY);
+  const cellColor = sampleColorFromDistance(gridDistance);
+
+  return grid.map((row, rowIndex) =>
+    row.map((cell, columnIndex) =>
+      columnIndex === clickX && rowIndex === clickY
+        ? { ...cell, color: cellColor }
+        : cell
     )
   );
 };
-
 function sampleColorFromDistance(distance: number): CellColor {
   const distanceKey =
     distance === 0
@@ -94,16 +92,20 @@ function sampleColorFromDistance(distance: number): CellColor {
       : distance === 4
       ? "4"
       : ">5";
+
   const probabilities = conditionalColorProbabilities[distanceKey];
+
   let cumulativeProbability = 0;
   const randomThreshold = Math.random();
+
   for (const color in probabilities) {
     cumulativeProbability += probabilities[color as CellColor];
     if (randomThreshold < cumulativeProbability) {
       return color as CellColor;
     }
   }
-  return "green"; // Default fallback
+
+  return "green";
 }
 
 export const UpdatePosteriorGhostLocationProbabilities = (
@@ -112,29 +114,29 @@ export const UpdatePosteriorGhostLocationProbabilities = (
   clickY: number,
   observedColor: CellColor
 ): GridCell[][] => {
-  let totalProbability = 0;
-  let updatedGrid = grid.map((row, y) =>
-    row.map((cell, x) => {
-      const distance = manhattanDistance(x, y, clickX, clickY);
-      const likelihood = jointTableProbability(observedColor, distance);
-      const newProbability = cell.probability * likelihood;
-      totalProbability += newProbability;
-      return { ...cell, probability: newProbability };
+  let totalProb = 0;
+  let gridUpdated = grid.map((row, rowIndex) =>
+    row.map((cell, colIndex) => {
+      const cellDistance = manhattanDistance(
+        colIndex,
+        rowIndex,
+        clickX,
+        clickY
+      );
+      const cellLikelihood = jointTableProbability(observedColor, cellDistance);
+      const updatedProb = cell.probability * cellLikelihood;
+      totalProb += updatedProb;
+      return { ...cell, probability: updatedProb };
     })
   );
 
-  if (totalProbability === 0)
-    return updatedGrid.map((row) =>
-      row.map((cell) => ({
-        ...cell,
-        probability: 1 / (gridSize.rows * gridSize.cols),
-      }))
-    );
+  const normalizationFactor =
+    totalProb === 0 ? gridSize.rows * gridSize.cols : totalProb;
 
-  return updatedGrid.map((row) =>
+  return gridUpdated.map((row) =>
     row.map((cell) => ({
       ...cell,
-      probability: cell.probability / totalProbability,
+      probability: cell.probability / normalizationFactor,
     }))
   );
 };
@@ -176,7 +178,6 @@ const jointTableProbability = (
   return 0;
 };
 
-// Grid component
 interface GridProps {
   grid: GridCell[][];
   onCellClick: (x: number, y: number) => void;
@@ -184,35 +185,29 @@ interface GridProps {
 }
 
 const Grid: React.FC<GridProps> = ({ grid, onCellClick, peep }) => {
-  const findBiggestNumber = (arr: GridCell[][]) => {
-    let biggest = 0;
-    arr.forEach((row) =>
-      row.forEach((cell) => {
-        if (cell.probability > biggest) biggest = cell.probability;
-      })
-    );
-    return biggest;
-  };
-
   return (
     <div
-      className="ma-w-5xl grid grid-cols-12 gap-1 bg-black p-1 rounded-md"
-      style={{ boxShadow: "0 0 50px 0 rgba(0,0,0,0.2)" }}
+      className="max-w-6xl grid grid-cols-12 gap-2 bg-gray-900 p-2 rounded-lg pb-3"
+      style={{ boxShadow: "0 0 20px 0 rgba(255,255,255,0.1)" }}
     >
-      {grid.map((row, y) =>
-        row.map((cell, x) => (
+      {grid.map((row, rowIndex) =>
+        row.map((cell, columnIndex) => (
           <button
-            key={`${x}-${y}`}
-            className={`w-16 h-10 flex items-center justify-center ${
-              cell.color ? `bg-${cell.color}-400` : "bg-white"
-            }  transition-all duration-150`}
+            key={`${columnIndex}-${rowIndex}`}
+            className={`w-16 h-12 flex items-center justify-center rounded transition-all duration-150 ${
+              cell.color ? `bg-${cell.color}-500` : "bg-gray-300"
+            }`}
             style={{
               backgroundColor:
                 cell.color && cell.color.length > 0 ? cell.color : "",
             }}
-            onClick={() => onCellClick(x, y)}
+            onClick={() => onCellClick(columnIndex, rowIndex)}
           >
-            {peep && `${cell.probability.toFixed(2)}`}
+            {peep && (
+              <span className="text-sm font-bold text-gray-900">
+                {cell.probability.toFixed(2)}
+              </span>
+            )}
           </button>
         ))
       )}
@@ -220,32 +215,20 @@ const Grid: React.FC<GridProps> = ({ grid, onCellClick, peep }) => {
   );
 };
 
-// Main game component (merged content)
-const NUMBER_OF_BUSTS = 2;
-const NUMBER_OF_CREDITS = 50;
+const BUSTS = 2;
+const CREDITS = 40;
+const Direc = "SE";
 
 export default function Home() {
   const router = useRouter();
   const [grid, setGrid] = React.useState<GridCell[][]>(initialGridState());
   const [ghost, setGhost] = React.useState<[number, number]>(placeGhost());
-  // const [lastClicked, setLastClicked] = React.useState<[number, number]>();
   const [lastClicked, setLastClicked] = React.useState<[number, number]>([
     0, 0,
   ]);
   const [peep, setPeep] = React.useState<boolean>(true);
-  const [remainingBusts, setRemainingBusts] =
-    React.useState<number>(NUMBER_OF_BUSTS);
-  const [credits, setCredits] = React.useState<number>(NUMBER_OF_CREDITS);
-  const {
-    isOpen: GameOverIsOpen,
-    onOpen: OnOpenGameOver,
-    onOpenChange: OnOpenGameOverChange,
-  } = useDisclosure();
-  const {
-    isOpen: WinIsOpen,
-    onOpen: OnOpenWin,
-    onOpenChange: OnOpenWinChange,
-  } = useDisclosure();
+  const [remainingBusts, setRemainingBusts] = React.useState<number>(BUSTS);
+  const [credits, setCredits] = React.useState<number>(CREDITS);
 
   const handleCellClick = (x: number, y: number) => {
     setLastClicked([x, y]);
@@ -261,164 +244,66 @@ export default function Home() {
     setGrid(updatedGrid);
   };
 
-  // React.useEffect(() => {
-  //   console.log("Grid", grid);
-  // }, [grid]);
-
   React.useEffect(() => {
-    if (credits === 0 || remainingBusts === 0) {
-      OnOpenGameOver();
+    if (credits === 0) {
+      alert("You have ran out of credits you lost the game");
+      router.reload();
+    } else if (remainingBusts === 0) {
+      alert("You have ran out of busts you lost the game");
+      router.reload();
     }
   }, [credits, remainingBusts]);
 
   return (
-    <div className="h-screen w-screen">
-      <div className="p-4 flex flex-col py-14 items-center justify-center">
-        <h1 className="text-center text-3xl font-bold mb-20">
-          Bust the Ghost Game
-        </h1>
-        <div className="flex flex-row gap-20">
+    <div className="h-screen w-screen bg-gray-700 text-white">
+      <div className="pt-10 flex flex-col items-center justify-center">
+        <div className="flex flex-wrap justify-center gap-10">
+          {/* Grid on the left */}
           <div className="flex flex-col gap-4">
+            <Grid grid={grid} onCellClick={handleCellClick} peep={peep} />
+          </div>
+          {/* Buttons on the right */}
+          <div className="flex flex-col gap-6">
+            <p>
+              Remaining Busts:{" "}
+              <span className="font-semibold">{remainingBusts}</span>
+            </p>
+            <p>
+              Credits: <span className="font-semibold">{credits}</span>
+            </p>
             <button
-              style={{ boxShadow: "0 0 50px 0 rgba(37,99,235,0.2)" }}
-              className="z-40 bg-gradient-to-br from-[#06b7f9] to-[#194aec] text-center text-white px-4 py-2 rounded-lg min-w-fit hover:brightness-95 transition-all duration-150"
-              onClick={() => {
-                setPeep(!peep);
-              }}
-            >
-              Peep
-            </button>
-            <button
-              className="z-40 bg-gradient-to-br from-red-500 to-red-800 text-center text-white px-8 py-2 rounded-lg min-w-fit hover:brightness-95 transition-all duration-150"
-              style={{ boxShadow: "0 0 50px 0 rgba(255, 0, 0, 0.2)" }}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out"
               onClick={() => {
                 if (remainingBusts === 0) return;
                 if (
                   lastClicked[0] === ghost[0] &&
                   lastClicked[1] === ghost[1]
                 ) {
-                  alert("congratulation you have won!");
+                  alert("Congratulations, you have won!");
                   router.reload();
                   return;
                 }
                 setRemainingBusts(remainingBusts - 1);
               }}
             >
-              Bust Ghost ({lastClicked[0]}, {lastClicked[1]})
+              Bust Ghost
             </button>
-            <p>
-              Remaining Busts:{" "}
-              <span className="font-bold">{remainingBusts}</span>
-            </p>
-            <p>
-              Credits: <span className="font-bold">{credits}</span>
-            </p>
-          </div>
-          <div className="flex flex-col gap-4">
-            <Grid grid={grid} onCellClick={handleCellClick} peep={peep} />
-            <div></div>
+            <h4 className="text-center">
+              Selected cell: row: {lastClicked[0] + 1}, column:{" "}
+              {lastClicked[1] + 1}
+            </h4>
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out"
+              onClick={() => setPeep(!peep)}
+            >
+              Peep
+            </button>
+            <div>
+              <p>direction = {Direc}</p>
+            </div>
           </div>
         </div>
       </div>
-      <Modal
-        backdrop="opaque"
-        isOpen={WinIsOpen}
-        onOpenChange={OnOpenWinChange}
-        hideCloseButton
-        isDismissable={false}
-        radius="lg"
-        classNames={{
-          body: "pt-2 pb-2",
-          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
-          base: "border-[#292f46] bg-white dark:bg-[#19172c] text-gray-700 max-w-xl",
-          closeButton: "hover:bg-white/5 active:bg-white/10",
-          footer: "pt-2",
-        }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex items-center justify-center gap-1">
-                <p className="text-2xl">You won!</p>
-              </ModalHeader>
-              <ModalBody className="pb-4">
-                <div className="flex flex-col">
-                  <p className="text-center mb-5">
-                    You have successfully busted the ghost. Congratulations!
-                  </p>
-                  <div className="flex items-center justify-center">
-                    <button
-                      style={{ boxShadow: "0 0 50px 0 rgba(37,99,235,0.2)" }}
-                      className="w-fit px-8 z-40 bg-gradient-to-br from-[#06b7f9] to-[#194aec] text-center text-white py-2 rounded-lg min-w-fit hover:brightness-95 transition-all duration-150"
-                      onClick={() => {
-                        setGrid(initialGridState());
-                        setCredits(NUMBER_OF_CREDITS);
-                        setRemainingBusts(NUMBER_OF_BUSTS);
-                        const [x, y] = placeGhost();
-                        setGhost([x, y]);
-                        onClose();
-                      }}
-                    >
-                      Restart
-                    </button>
-                  </div>
-                </div>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-      <Modal
-        backdrop="opaque"
-        isOpen={GameOverIsOpen}
-        onOpenChange={OnOpenGameOverChange}
-        hideCloseButton
-        isDismissable={false}
-        radius="lg"
-        classNames={{
-          body: "pt-2 pb-2",
-          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
-          base: "border-[#292f46] bg-white dark:bg-[#19172c] text-gray-700 max-w-xl",
-          closeButton: "hover:bg-white/5 active:bg-white/10",
-          footer: "pt-2",
-        }}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex items-center justify-center gap-1">
-                <p className="text-2xl">Game Over</p>
-              </ModalHeader>
-              <ModalBody className="pb-4">
-                <div className="flex flex-col">
-                  <p className="text-center">
-                    You have run out of credits. Restart the game to play again.
-                  </p>
-                  <p className="font-semibold text-center mb-5">
-                    (Ghost was at {ghost[0]}, {ghost[1]})
-                  </p>
-                  <div className="flex items-center justify-center">
-                    <button
-                      style={{ boxShadow: "0 0 50px 0 rgba(37,99,235,0.2)" }}
-                      className="w-fit px-8 z-40 bg-gradient-to-br from-[#06b7f9] to-[#194aec] text-center text-white py-2 rounded-lg min-w-fit hover:brightness-95 transition-all duration-150"
-                      onClick={() => {
-                        setGrid(initialGridState());
-                        setCredits(NUMBER_OF_CREDITS);
-                        setRemainingBusts(2);
-                        const [x, y] = placeGhost();
-                        setGhost([x, y]);
-                        onClose();
-                      }}
-                    >
-                      Restart
-                    </button>
-                  </div>
-                </div>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
     </div>
   );
 }
